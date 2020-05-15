520ffafdcb7fb60ff623e3a50e0e8c9a94e46106
vsrn09 <venkat.namala@ibm.com>
2020-05-15 12:59:36 -0500

    first commit

--- install.sh
+++ install.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 set -e
+set -x
 CPK4MCM_FQDN=$(oc get routes -n kube-system icp-console -o jsonpath='{.spec.host}')
 ICP_ADMIN_PASSWORD=$(oc -n kube-system get secret platform-auth-idp-credentials -o jsonpath="{.data.admin_password}" | base64 --decode)
 
-cloudctl login "https://${CPK4MCM_FQDN}" --skip-ssl-validation -n default -u admin -p ${ICP_ADMIN_PASSWORD}
+cloudctl login -a "https://${CPK4MCM_FQDN}" --skip-ssl-validation -n default -u admin -p ${ICP_ADMIN_PASSWORD}
 helm package guestbook/gbapp
 
 helm package guestbook/gbchn
